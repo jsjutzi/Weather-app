6bf6c70531f0cd1951b6d10d48e68c408231b3a4
Jack <jsjutzi@gmail.com>
2017-12-05 16:01:32 -0600

    fully functional

--- src/App.js
+++ src/App.js
@@ -7,33 +7,29 @@ class App extends Component {
     super(props);
     this.state = {
       currentWeather: {},
-      forecastedWeather: [],
-      searchCity: " "
+      condition: "",
+      conditionDescription: "",
+      temp: "",
+      searchCity: " ",
+      cardVisible: "hide",
+      windSpeed: "",
+      windChill: "",
+      icon: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.getForecast = this.getForecast.bind(this);
+    this.convertTemp = this.convertTemp.bind(this);
+  }
+  convertTemp(temp) {
+    return 1.8 * (temp - 273) + 32;
   }
 
   handleChange(val) {
     this.setState({ searchCity: val });
     console.log(this.state.searchCity);
   }
-  getExtendedForecast() {
-    axios
-      .get(
-        `http://api.openweathermap.org/data/2.5/forecast?zip=${
-          this.state.searchCity
-        }&appid=39b0a9078bc3441996350bb20ac1bd36`
-      )
-      .then(response => {
-        this.setState({ forecastedWeather: response });
-        console.log(response);
-        console.log("this is the state", this.state.forecastedWeather);
-      })
-      .catch(err => err);
-  }
+
   getForecast() {
-    console.log(this.state.searchCity);
     axios
       .get(
         `http://api.openweathermap.org/data/2.5/weather?zip=${
@@ -41,12 +37,22 @@ class App extends Component {
         }&appid=39b0a9078bc3441996350bb20ac1bd36`
       )
       .then(response => {
-        this.setState({ currentWeather: response });
-        console.log(response);
-        console.log("this is the state", this.state.currentWeather);
+        this.setState({
+          currentWeather: response.data,
+          temp: response.data.main.temp,
+          condition: response.data.weather[0].main,
+          conditionDescription: response.data.weather[0].description,
+          windSpeed: response.data.wind.speed,
+          windChill: response.data.wind.deg,
+          icon: `http://openweathermap.org/img/w/${
+            response.data.weather[0].icon
+          }.png`,
+          cardVisible: "show"
+        });
+        console.log("here is the new state", this.state);
+        //console.log("this is the state", this.state.currentWeather);
       })
       .catch(err => err);
-    this.getExtendedForecast();
   }
 
   render() {
@@ -73,6 +79,23 @@ class App extends Component {
         >
           Forecast
         </button>
+        <p id="city">{this.state.currentWeather.name}</p>
+        <div id="current-forecast">
+          <p id="type" />
+        </div>
+        <div id="weather-box" className={this.state.cardVisible}>
+          <p className="weather-box-text" id="conditions">
+            {this.state.condition}
+          </p>
+          <img src={this.state.icon} id="icon" />
+          <p className="weather-box-text" id="degrees">
+            {Math.floor(this.convertTemp(this.state.temp))}
+            &#8457;
+          </p>
+          <p className="weather-box-text" id="wind-speed">
+            Wind: {this.state.windSpeed} mph
+          </p>
+        </div>
       </div>
     );
   }
---
+++ src/weatherCard.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import("./weatherCard.css");
+
+class WeatherCard extends Component {
+  render() {
+    return <p id="temp" />;
+  }
+}
